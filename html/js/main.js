9319fdb32aca1211943b8e28a92c5b10bbc1ca6b
RxPro <wxweb@mail.ru>
2022-04-27 07:14:50 +0300

    Верстка страницы Сервиса с адаптивом. Жду от дизайнера следующих страниц

--- html/js/main.js
+++ html/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   // Your code here
   financeSlider();
   menuToggle();
+  featuresSlider();
 }); // Your functions here
 
 function financeSlider() {
@@ -23,6 +24,15 @@ function menuToggle() {
     $(headerBurger).on('click', function (e) {
       $(header).toggleClass('header__inner--active');
       $(body).toggleClass('no-scroll');
+    });
+  }
+}
+
+function featuresSlider() {
+  if ($(window).width() < 768) {
+    var features = new Swiper(".features__list", {
+      slidesPerView: "auto",
+      spaceBetween: 12
     });
   }
 }
--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     // Your code here
     financeSlider()
     menuToggle()
+    featuresSlider()
 });
 
 // Your functions here
@@ -22,9 +23,18 @@ function menuToggle() {
         let headerBurger = document.querySelector('.header__burger');
         let header = document.querySelector('.header__inner');
         let body = document.querySelector('body');
-        $(headerBurger).on('click', function(e) {
+        $(headerBurger).on('click', function (e) {
             $(header).toggleClass('header__inner--active');
             $(body).toggleClass('no-scroll');
         })
+    }
+}
+
+function featuresSlider() {
+    if ($(window).width() < 768) {
+        let features = new Swiper(".features__list", {
+            slidesPerView: "auto",
+            spaceBetween: 12,
+        });
     }
 }
